119dfb5c07da06a8f2a27dc69cc3394f28a2584e
Gian López <lopezarizagianlucas@gmail.com>
2021-08-05 23:54:55 -0500

    master: the queue is downloading the songs in the correct folder, i need to make it async.

--- src/services/download.js
+++ src/services/download.js
@@ -1,15 +1,17 @@
+import { store } from '../redux/store';
 const ytdl = window.require('ytdl-core');
 const fs = window.require('fs');
+const path = window.require('path');
 
 class DownloadService {
-
+  
   async get_mp3(url, title) {
+    let { folder = '' } = store.getState()?.package,
+    songPath = path.join(folder, `${title}.mp3`);
     await ytdl(url, { quality: 'highestaudio', filter: 'audioonly' })
-      .pipe(fs.createWriteStream(`${title}.mp3`));
-    // debugger
+      .pipe(fs.createWriteStream(songPath));
   };
 
-  
 };
 
 export { DownloadService };
