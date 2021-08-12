67fc1e361ce908b65ddd9196f4817c993493b7a7
Gian López <lopezarizagianlucas@gmail.com>
2021-08-12 17:43:50 -0500

    master: i need to look for some way to add the songs cover.

--- src/services/download.js
+++ src/services/download.js
@@ -7,50 +7,40 @@ const path = window.require('path');
 
 class Download {
 
-  constructor(stateManager) {
-    this.stateManager = stateManager;
+  constructor(manager) { this.manager = manager };
+
+  getPercentage = (dld, total) => parseInt(100 / total * dld);
+
+  converter(folder, toconvert, title) {
+    let tagged = path.join(folder, `${title}.mp3`);
+    ffmpeg.setFfmpegPath(ffmpegPath);
+    ffmpeg(toconvert).output(tagged)
+      .on('error', err => console.error(err)).run()
+      .on('end', () => fs.unlink(toconvert, _ => {}));
   };
 
-  getProgress = (dld, total) => parseInt(100 / total * dld);
-
-  formatTitle(title) {
-    let newTitle = '',
-    invalid = ['\\', '/', ':', '*', '?', '"', '<', '>', '|'];
-    for (let char of title) {
-      newTitle = invalid.includes(char) ? '' : char;
-    };
-    return newTitle;
-  };
-
-  streamHandler(url, outfolder, title) {
+  downloader(url, outfolder, title) {
     return new Promise((res, rej) => {
-      let safeTitle = title.replace(/\\|\/|:|\*|\?|"|<|>|\|:/g, ''),
-      song = path.join(outfolder, `${safeTitle} - Untaggeable.mp3`);
+      let song = path.join(outfolder, `${title} - Untaggeable.mp3`);
       ytdl(url, { quality: 'highestaudio', filters: 'audioonly' })
         .on('progress', (_, num1, num2) => {
-          let progress = this.getProgress(num1, num2);
-          this.stateManager({ progress });
-        }).on('finish', () => {
-          let tagged = path.join(outfolder, `${safeTitle}.mp3`);
-          ffmpeg.setFfmpegPath(ffmpegPath);
-          ffmpeg(song).output(tagged)
-            .on('end', () => {
-              fs.unlink(song, (err) => {
-                if (err) throw err;
-                res();
-              });
-            })
-            .on('error', err => console.error(err))
-            .run();
-        }).pipe(fs.createWriteStream(song));
+          let progress = this.getPercentage(num1, num2);
+          this.manager({ progress });
+        })
+        .on('finish', () => {
+          this.converter(outfolder, song, title);
+          res();
+        })
+        .pipe(fs.createWriteStream(song));
     });
   };
 
   async get_mp3(url = '', title) {
-    let { folder = '' } = store.getState()?.package,    
-    valid = ytdl.validateURL(url);
+    let { folder = '' } = store.getState()?.package,
+    _title = title.replace(/\\|\/|:|\*|\?|"|<|>|\|:/g, ''),
+    valid = ytdl.validateURL(url)
     if (valid) {
-      await this.streamHandler(url, folder, title);
+      await this.downloader(url, folder, _title);
       return path.join(folder, `${title}.mp3`);
     };
   };
